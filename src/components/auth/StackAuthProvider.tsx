a0f01a73b2d3f3c99d3576c29cb866bd573fb4be
MikaelTHEoret <Mikaeltheoret@gmail.com>
2025-06-14 00:56:30 -0400

    HOTFIX: Fix Stack Auth API change - use app prop instead of individual props

--- src/components/auth/StackAuthProvider.tsx
+++ src/components/auth/StackAuthProvider.tsx
@@ -1,14 +1,11 @@
 'use client'
 
 import { StackProvider } from '@stackframe/stack'
+import { stackServerApp } from '@/stack'
 
 export default function StackAuthProvider({ children }: { children: React.ReactNode }) {
   return (
-    <StackProvider
-      appId="st_tcutrWqiStGLyVSB" // Demo project ID from original
-      appUrl={process.env.NEXT_PUBLIC_APP_URL || 'http://localhost:3000'}
-      tokenStore="nextjs-cookie"
-    >
+    <StackProvider app={stackServerApp}>
       {children}
     </StackProvider>
   )
