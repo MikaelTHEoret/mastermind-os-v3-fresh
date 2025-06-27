166fb798a169dbca0eb8bf6abd06485084f917d3
MikaelTHEoret <Mikaeltheoret@gmail.com>
2025-06-27 09:56:23 -0400

    🗑️ REMOVE CONFLICTING PAGES: Delete pages/index.js to allow App Router to work

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,1 @@
-export default function Home() {
-  return (
-    <div>
-      <h1>MASTERMIND OS v3.0</h1>
-      <p>🧠 Enhanced Nexus Core Protocol v6.0</p>
-      <p>✅ Application Successfully Deployed</p>
-    </div>
-  )
-}
+temp
